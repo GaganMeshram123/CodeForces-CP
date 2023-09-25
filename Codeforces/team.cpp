087bef94ce780aaf1e4d367e3b7ab3d4a3c3cb78
Gagan Hariram Meshram <109589418+GaganMeshram123@users.noreply.github.com>
2023-09-25 11:54:18 +0530

    Add files via upload

---
+++ Codeforces/team.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() { 
+     int t;
+     cin>>t;
+     while(t--){
+     string str;
+     cin>>str;
+     int len=str.size();
+     if(len>10){
+     cout<<str[0]<<(len-2)<<str[len-1]<<endl;
+      else{
+         cout<<str<<endl;
+      }
+     }
+     return 0;
+     }
+     
+    }      }
+      }
---
+++ Codeforces/waytolongnumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() { 
+    int n;
+    cin>>n;
+    cin.ignore();//this is most imp because it consumes the newline after reading n 
+
+    for( int i=0;i<=n;++i){
+       //ithe apan string call kartoy 
+      string word;
+      getline(cin,word);//here the code says it will read as the netire code 
+      cout<<" word is "<<endl;
+      }
+      return 0;
+    }
